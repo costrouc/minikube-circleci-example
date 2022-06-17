ac03a8483eb802d025e197b92c51789ecdd92da4
Chris Ostrouchov <chris.ostrouchov@gmail.com>
2022-06-17 17:33:05 -0400

    Adding z2jh deployment

--- .circleci/test.sh
+++ .circleci/test.sh
@@ -9,8 +9,9 @@ set -x;
 
 # working dir to install binaries etc, cleaned up on exit
 BIN_DIR="$(mktemp -d)"
-# minikube binary will be here
+# binaries will be here
 MINIKUBE="${BIN_DIR}/minikube"
+HELM="${BIN_DIR}/helm"
 
 # cleanup on exit (useful for running locally)
 cleanup() {
@@ -19,21 +20,38 @@ cleanup() {
 }
 trap cleanup EXIT
 
-# util to install a released minikube version into ${BIN_DIR}
 install_minikube_release() {
     MINIKUBE_BINARY_URL="https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64"
     wget -O "${MINIKUBE}" "${MINIKUBE_BINARY_URL}"
     chmod +x "${MINIKUBE}"
 }
 
+install_helm_release() {
+    HELM_BINARY_URL="https://get.helm.sh/helm-v3.9.0-linux-amd64.tar.gz"
+    wget -O "${HELM}" "${MINIKUBE_BINARY_URL}"
+    chmod +x "${HELM}"
+}
+
 main() {
-    # get minikube
+    # get binaries
     install_minikube_release
+    install_helm_release
+
     # create a cluster
     "${MINIKUBE}" start --force --wait=all
 
-    # create resources
+    # create kustomize resources
     "${MINIKUBE}" kubectl -- apply -k resources
+
+    # deploy z2jh helm chart
+    "${HELM}" repo add jupyterhub https://jupyterhub.github.io/helm-chart/
+    "${HELM}" repo update
+    "${HELM}" upgrade --cleanup-on-fail \
+              --install z2jh jupyterhub/jupyterhub \
+              --namespace default \
+              --create-namespace \
+              --version=1.2.0 \
+              --wait
 
     # wait to resources to start running
     "${MINIKUBE}" kubectl -- wait pods -n default -l app=postgres --for condition=Ready --timeout=90s
