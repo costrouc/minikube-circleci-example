dd394b59a3126ea7713df3c94bc54fbb0f7131e4
Chris Ostrouchov <chris.ostrouchov@gmail.com>
2022-06-17 17:49:09 -0400

    Modifying test

--- .circleci/test.sh
+++ .circleci/test.sh
@@ -9,7 +9,9 @@ set -x;
 
 # working dir to install binaries etc, cleaned up on exit
 BIN_DIR="$(mktemp -d)"
+
 # binaries will be here
+KUBECTL="${BIN_DIR}/kubectl"
 MINIKUBE="${BIN_DIR}/minikube"
 HELM="${BIN_DIR}/helm"
 
@@ -26,6 +28,12 @@ install_minikube_release() {
     chmod +x "${MINIKUBE}"
 }
 
+install_kubectl_release() {
+    KUBECTL_BINARY_URL="https://dl.k8s.io/release/v1.24.0/bin/linux/amd64/kubectl"
+    wget -O "${KUBECTL}" "${KUBECTL_BINARY_URL}"
+    chmod +x "${KUBECTL}"
+}
+
 install_helm_release() {
     HELM_BINARY_URL="https://get.helm.sh/helm-v3.9.0-linux-amd64.tar.gz"
     wget -O "${HELM}.tar.gz" "${HELM_BINARY_URL}"
@@ -37,13 +45,14 @@ install_helm_release() {
 main() {
     # get binaries
     install_minikube_release
+    install_kubectl_release
     install_helm_release
 
     # create a cluster
     "${MINIKUBE}" start --force --wait=all
 
     # create kustomize resources
-    "${MINIKUBE}" kubectl -- apply -k resources
+    "${KUBECTL}" apply -k resources
 
     # deploy z2jh helm chart
     "${HELM}" repo add jupyterhub https://jupyterhub.github.io/helm-chart/
@@ -52,13 +61,12 @@ main() {
               --install z2jh jupyterhub/jupyterhub \
               --namespace default \
               --create-namespace \
-              --version=1.2.0 \
-              --wait
+              --version=1.2.0
 
     # wait to resources to start running
-    "${MINIKUBE}" kubectl -- wait pods -n default -l app=postgres --for condition=Ready --timeout=90s
+    "${KUBECTL}" wait pods -n default -l app=postgres --for condition=Ready --timeout=90s
 
-    "${MINIKUBE}" kubectl -- get pods -A
+    "${KUBECTL}" get pods -A
     # TODO: invoke your tests here
     # teardown will happen automatically on exit
 }
